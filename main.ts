0e3888b8575f02a72d42df037a05ca823b93a2ce
Jens Dalsgaard Nielsen <jdn@es.aau.dk>
2022-09-29 22:30:41 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,20 @@ cuteBot.forward()
 basic.pause(100)
 cuteBot.stopcar()
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        . . # . .
+        . # . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . .
+        `)
+    basic.pause(1000)
 })
